2887a25ef1c370c206f277557f9917d1815e412a
Ninelove <bilik.sergey66@gmail.com>
2025-04-23 23:09:33 +0300

    add secret keys

--- configs/wdio.conf.js
+++ configs/wdio.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: "user.game17@gmail.com",
-  key: "Xz5p6Mt2LQTati8tfp9X",
+  user: process.env.BROWSERSTACK_USERNAME, 
+  key: process.env.BROWSERSTACK_ACCESS_KEY,
 
   updateJob: false,
   specs: ["../test/specs/**.e2e.js"],
